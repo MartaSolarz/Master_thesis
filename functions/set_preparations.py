69acb6968bbdc205c0442529956baf6515880390
msolarzwebsensa <msolarzwebsensa@dcwp.pl>
2023-10-22 21:02:36 +0100

    general: eye-tracking-metrics

--- functions/set_preparations.py
+++ functions/set_preparations.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+
 def prepare_row(list_of_cols):
     """
     Prepare single row for data set.
@@ -25,3 +26,20 @@ def prepare_set_changes_questions(list_of_cols, quest1, quest2, name_list_of_col
         new_row_quest2 = pd.concat([new_row_quest2, i], ignore_index=True)
 
     return pd.DataFrame({name_list_of_cols: new_row, 'quest1': new_row_quest1, 'quest2': new_row_quest2})
+
+
+def sum_rows(row, pattern, specify, flag=False):
+    if flag:
+        pref = pattern
+        suf = ''
+    else:
+        pref, suf = pattern.split()
+    el1 = pref + specify[0] + suf
+    el2 = pref + specify[1] + suf
+    if len(specify) == 3:
+        el3 = pref + specify[2] + suf
+        return row[el1] + row[el2] + row[el3]
+    elif len(specify) == 2:
+        return row[el1] + row[el2]
+    else:
+        return ValueError
